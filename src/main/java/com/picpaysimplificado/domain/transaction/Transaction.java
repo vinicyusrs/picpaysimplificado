62569d587ffd608451ee113b97014136830812af
vinicyus.rs@gmail.com <vinicyus.rs@gmail.com>
2024-02-27 20:38:42 -0300

    create: finalizacao da classe Transaction

--- src/main/java/com/picpaysimplificado/domain/transaction/Transaction.java
+++ src/main/java/com/picpaysimplificado/domain/transaction/Transaction.java
@@ -1,9 +1,15 @@
 package com.picpaysimplificado.domain.transaction;
 
+import java.math.BigDecimal;
+
+import com.picpaysimplificado.domain.user.User;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
@@ -21,7 +27,14 @@ public class Transaction {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	private BigDecimal amount;
+	@ManyToOne
+	@JoinColumn(name="sender_id")
+	private User sender;
 	
+	@ManyToOne
+	@JoinColumn(name="receiver_id")
+	private User receiver;
 
 
 }
